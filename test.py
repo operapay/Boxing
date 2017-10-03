477c247081635e824fef4226986b7d2a466499cc
operapay <opera.pay@gmail.com>
2017-10-03 16:23:18 +0700

    fallling

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import arcade
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 700
 
 
 class boxing(arcade.Window):
@@ -11,15 +11,24 @@ class boxing(arcade.Window):
         arcade.set_background_color(arcade.color.BLACK)
 
         self.frame_count = 0
+        self.frame_count1 = 0
 
         self.sprites_list = arcade.SpriteList()
         self.character_list = arcade.SpriteList()
         self.bullet_list = arcade.SpriteList()
+        self.enemy_list = arcade.SpriteList()
 
         self.character = arcade.Sprite("images/boy.png")
-
         self.sprites_list.append(self.character)
         self.character_list.append(self.character)
+
+        for i in range(8):
+            self.enemy = arcade.Sprite("images/girl.png", 0.5)
+            self.enemy.center_x = SCREEN_WIDTH - 70*i
+            self.enemy.center_y = SCREEN_HEIGHT + 50
+            self.enemy.angle = 0
+            self.sprites_list.append(self.enemy)
+            self.enemy_list.append(self.enemy)
 
     def on_draw(self):
         arcade.start_render()
@@ -37,6 +46,18 @@ class boxing(arcade.Window):
                 bullet.change_y = 5
                 self.bullet_list.append(bullet)
                 self.sprites_list.append(bullet)
+
+        self.frame_count1 += 1
+
+        for self.enemy in self.enemy_list:
+            if self.frame_count1 % 180 == 0:
+                bullet1 = arcade.Sprite("images/girl.png",0.7)
+                bullet1.center_x = self.enemy.center_x
+                bullet1.angle = 0
+                bullet1.top = self.enemy.bottom
+                bullet1.change_y = -3
+                self.bullet_list.append(bullet1)
+                self.sprites_list.append(bullet1)
 
         for bullet in self.bullet_list:
             if bullet.top < 0:
